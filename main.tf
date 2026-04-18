4e19772a29ab9dbf9e9883d700e2626da9088219
Cristian Beraha <cristian@cloudops.works>
2026-04-18 00:06:30 -0300

    feat: initial implementation of Azure MySQL Flexible Server module
    Implements provisioning module for Azure MySQL Flexible Server with
    Key Vault credential storage, Hoop connection output (enterprise mode),
    firewall rules, and configuration management.

--- main.tf
+++ main.tf
@@ -6,3 +6,95 @@
 #       WebSite: https://cloudops.works
 #     Distributed Under Apache v2.0 License
 #
+
+locals {
+  server_name   = try(var.settings.name_prefix, "") != "" ? "${local.system_name_short}-${var.settings.name_prefix}" : local.system_name_short
+  database_name = try(var.settings.database_name, "mysql")
+  admin_login   = try(var.settings.administrator_login, "adminuser")
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "${local.system_name}-mysql-rg"
+  location = var.region
+  tags     = local.all_tags
+}
+
+resource "random_password" "master" {
+  length           = 20
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+  min_lower        = 2
+  min_upper        = 2
+  min_numeric      = 2
+  min_special      = 2
+}
+
+resource "azurerm_mysql_flexible_server" "this" {
+  name                         = local.server_name
+  resource_group_name          = azurerm_resource_group.this.name
+  location                     = azurerm_resource_group.this.location
+  version                      = var.settings.version
+  sku_name                     = var.settings.sku_name
+  backup_retention_days        = try(var.settings.backup_retention_days, 7)
+  geo_redundant_backup_enabled = try(var.settings.geo_redundant_backup, "Disabled") == "Enabled"
+  zone                         = try(var.settings.zone, null)
+  administrator_login          = local.admin_login
+  administrator_password       = random_password.master.result
+  delegated_subnet_id          = try(var.network.delegated_subnet_id, null)
+  private_dns_zone_id          = try(var.network.private_dns_zone_id, null)
+  tags                         = local.all_tags
+
+  storage {
+    size_gb           = try(var.settings.storage_mb, 20480) / 1024
+    iops              = try(var.settings.storage_iops, 396)
+    auto_grow_enabled = try(var.settings.storage_auto_grow, "Enabled") == "Enabled"
+  }
+
+  dynamic "high_availability" {
+    for_each = try(var.settings.high_availability.enabled, false) ? [1] : []
+    content {
+      mode                      = try(var.settings.high_availability.mode, "ZoneRedundant")
+      standby_availability_zone = try(var.settings.high_availability.standby_zone, null)
+    }
+  }
+
+  maintenance_window {
+    day_of_week  = try(var.settings.maintenance.day_of_week, 0)
+    start_hour   = try(var.settings.maintenance.start_hour, 0)
+    start_minute = try(var.settings.maintenance.start_minute, 0)
+  }
+}
+
+resource "azurerm_mysql_flexible_database" "initial" {
+  name                = local.database_name
+  resource_group_name = azurerm_resource_group.this.name
+  server_name         = azurerm_mysql_flexible_server.this.name
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_unicode_ci"
+}
+
+resource "azurerm_mysql_flexible_database" "additional" {
+  for_each            = try(var.settings.databases, {})
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.this.name
+  server_name         = azurerm_mysql_flexible_server.this.name
+  charset             = try(each.value.charset, "utf8mb4")
+  collation           = try(each.value.collation, "utf8mb4_unicode_ci")
+}
+
+resource "azurerm_mysql_flexible_server_configuration" "params" {
+  for_each            = { for p in try(var.settings.parameters, []) : p.name => p }
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.this.name
+  server_name         = azurerm_mysql_flexible_server.this.name
+  value               = each.value.value
+}
+
+resource "azurerm_mysql_flexible_server_firewall_rule" "this" {
+  for_each            = { for r in try(var.network.firewall_rules, []) : r.name => r }
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.this.name
+  server_name         = azurerm_mysql_flexible_server.this.name
+  start_ip_address    = each.value.start_ip
+  end_ip_address      = each.value.end_ip
+}
